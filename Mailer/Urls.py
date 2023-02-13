6d140ad3265c6c2ed53ca86660b1ef25b9b5d9d5
Silvester Joseph <Silvester.Joseph@equiniti.com>
2023-02-13 15:44:10 +0530

    Chamge in UI

--- Mailer/Urls.py
+++ Mailer/Urls.py
@@ -3,4 +3,7 @@ from Mailer import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("hello/<name>", views.helloviewer, name="helloviewer"),
+    path("mail", views.reademail, name="reademail"),
+    path("compose", views.composemail, name="composemail")
 ]
--- Mailer/views.py
+++ Mailer/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.utils.timezone import datetime
+import re
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("Hello World From Django!!!")
+
+def helloviewer(request, name):
+    return render(
+        request,
+        'Mailer/hellothere.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+def reademail(request):
+    return render(
+        request,
+        'Mailer/openemail.html'
+    )
+
+def composemail(request):
+    return render(
+        request,
+        'Mailer/ComposeMail.html'
+    )
